27d7220b0dca33cfe75cac423cf981a73d3feca0
Drew Cook <drew@dco.dev>
2023-07-01 00:45:39 -0600

    feat: game dashboard UI

--- src/components/GameDashboard.tsx
+++ src/components/GameDashboard.tsx
@@ -13,11 +13,8 @@ type GameDashboardProps = {
 const GameDashboard = ({ accountStatus }: GameDashboardProps): JSX.Element => {
 	const {
 		connectedAccount,
-		contracts,
 		/*sendTxLoading, sendTxError*/
 	} = useWeb3()
-
-	console.log(typeof contracts?.rps, typeof contracts?.hasher)
 
 	// Fallback
 	if (!connectedAccount) return <Typography>Connect your wallet to view your game</Typography>
@@ -26,29 +23,77 @@ const GameDashboard = ({ accountStatus }: GameDashboardProps): JSX.Element => {
 		<Box>
 			{/* {sendTxLoading && <Typography>Transaction is loading...</Typography>}
 			{sendTxError && <Typography color="error">Transaction Error: {sendTxError.toString()}</Typography>} */}
-			<Typography variant="h3" textAlign="center" gutterBottom>
+			<Typography variant="h3" textAlign="center">
 				Current Game
-			</Typography>
-			<Typography mt={4} mb={4}>
-				Game Contract Address:{' '}
-				<Link href={`https://sepolia.etherscan.io/address/${connectedAccount.gameAddress}`} target="_blank">
-					<Typography
-						component="span"
-						display="inline-flex"
-						alignItems="center"
-						justifyContent="center"
-						flexDirection="row"
-					>
-						View on Sepolia <OpenInNew />
-					</Typography>
-				</Link>
 			</Typography>
 			<Typography mb={4}>Connected Account Status: {accountStatus}</Typography>
 			<Grid container spacing={2} mt={4}>
-				<Grid item xs={12} sm={6}>
-					<PlayerMoves name="Player 1" />
+				<Grid item xs={12} md={6}>
+					{/* TODO: Read data from contract, i.e. stake, players, hash, etc */}
+					<Typography variant="h4" textAlign="center" mb={4}>
+						Game Data
+					</Typography>
+					<Box textAlign="left">
+						<Typography mb={2}>
+							Game Address:
+							<br />
+							<Link href={`https://sepolia.etherscan.io/address/${connectedAccount.gameAddress}`} target="_blank">
+								<Typography
+									component="span"
+									display="inline-flex"
+									alignItems="center"
+									justifyContent="center"
+									flexDirection="row"
+								>
+									{connectedAccount.gameAddress} <OpenInNew />
+								</Typography>
+							</Link>
+						</Typography>
+						<Typography mb={2}>
+							Game Stake:
+							<br />
+							{connectedAccount.stake} ETH
+						</Typography>
+						<Typography mb={2}>
+							Player 1:
+							<br />
+							<Link href={`https://sepolia.etherscan.io/address/${connectedAccount.player}`} target="_blank">
+								<Typography
+									component="span"
+									display="inline-flex"
+									alignItems="center"
+									justifyContent="center"
+									flexDirection="row"
+								>
+									{connectedAccount.player}
+									<OpenInNew />
+								</Typography>
+							</Link>
+						</Typography>
+						<Typography mb={2}>
+							Player 2:
+							<br />
+							<Link href={`https://sepolia.etherscan.io/address/${connectedAccount.opponent}`} target="_blank">
+								<Typography
+									component="span"
+									display="inline-flex"
+									alignItems="center"
+									justifyContent="center"
+									flexDirection="row"
+								>
+									{connectedAccount.opponent}
+									<OpenInNew />
+								</Typography>
+							</Link>
+						</Typography>
+						<Typography mb={2}>
+							Player 1 Identity Commitment:
+							<br />
+							{connectedAccount.c1Hash}
+						</Typography>
+					</Box>
 				</Grid>
-				<Grid item xs={12} sm={6}>
+				<Grid item xs={12} md={6}>
 					<PlayerMoves name="Player 2" />
 				</Grid>
 			</Grid>
--- src/components/PlayerMoves.tsx
+++ src/components/PlayerMoves.tsx
@@ -1,10 +1,20 @@
 'use client'
 import { Button, Paper, Typography } from '@mui/material'
+import { format } from 'date-fns'
+import { useState } from 'react'
+import { parseEther } from 'viem'
+
+import { Move } from '@/lib/types'
+
+import { useWeb3 } from './Web3Provider'
 
 const styles = {
 	paper: {
 		p: 2,
 		m: 2,
+	},
+	btn: {
+		mb: 2,
 	},
 }
 
@@ -13,15 +23,37 @@ type PlayerMovesProps = {
 }
 
 const PlayerMoves = (props: PlayerMovesProps): JSX.Element => {
-	const handleMakeMove = () => {
-		console.log(`${props.name} make move`)
+	const { contracts } = useWeb3()
+	const [lastAction, setLastAction] = useState<string>('')
+
+	// For any player to get the timestamp of the last action
+	const handleGetLastAction = async () => {
+		const lastActionTimeStamp = await contracts?.rps.read.lastAction()
+		setLastAction(format(new Date(Number(lastActionTimeStamp)), 'yyyy-MM-dd HH:mm:ss'))
 	}
+
+	// For opponent to make their move
+	const handleMakeMove = async (move: Move) => {
+		console.log('contract', contracts?.rps)
+		const response = await contracts?.rps.simulate.play({ args: [move], value: parseEther('0.38') })
+		console.log({ response })
+	}
+
 	return (
 		<Paper elevation={2} sx={styles.paper}>
-			<Typography variant="h3">Player Moves</Typography>
-			<Typography variant="h4">{props.name}</Typography>
-			<Button variant="contained" onClick={handleMakeMove}>
-				Make A Move
+			<Typography variant="h4" mb={4}>
+				Available Actions
+			</Typography>
+			{lastAction && (
+				<Typography variant="caption" mb={2} mt={2}>
+					Last Action: {lastAction}
+				</Typography>
+			)}
+			<Button variant="contained" onClick={handleGetLastAction} fullWidth sx={styles.btn}>
+				Get Last Action
+			</Button>
+			<Button variant="contained" onClick={() => handleMakeMove(Move.Spock)} fullWidth sx={styles.btn}>
+				Make Move
 			</Button>
 		</Paper>
 	)
